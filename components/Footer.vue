e9d7117217f76fbad1126ce33c7a86c05c5e7b8d
Milly98 <milly98@naver.com>
2024-06-10 16:33:59 +0900

    대표이사 변경

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="container px-5" style=" padding-top: 2.5rem">
       <!-- <h4 id="im7hsh" style="text-align: left; color: var(--portalThemeColor8); padding-top: 2rem">Contact&nbsp;</h4> -->
       <p id="ib14zd">
-(주)이오그램 | 대표이사 : 임선희 | 사업자 등록번호 : 372-88-02084<br></p>
+(주)이오그램 | 대표이사 : 박영학 | 사업자 등록번호 : 372-88-02084<br></p>
 <p>주소 : 서울특별시 강동구 올림픽로 651 9층 (주)이오그램</p>
         <p>메일 : eogram@25gram.com | 연락처 : 82+ 02-6949-0814</p>
       <div id="i9d5ur" class="sectionBlockLayout" style="display: flex; flex-wrap: wrap; padding: 8px; margin: 0px; min-height: 15px;"></div>
